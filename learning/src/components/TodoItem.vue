db3b4551f228b57f653c7feb52239ee6d848e4eb
houssi27 <houssi27@hotmail.com>
2021-03-25 14:53:11 +0100

    API

--- learning/src/components/TodoItem.vue
+++ learning/src/components/TodoItem.vue
@@ -3,13 +3,19 @@
         <p>
             <input type="checkbox" v-on:change="markComplete">
             {{todo.title }}
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
         </p>
     </div>
 </template>
 <script>
 export default {
     name:"TodoItem",
-    props: ["todo"]
+    props: ["todo"],
+    methods:{
+        markComplete(){
+            this.todo.completed = !this.todo.completed;
+        }
+    }
 }
 </script>
 <style scope>
