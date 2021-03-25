49b78cb30dbe6121090f2bf174346f05aa7b955d
houssi27 <houssi27@hotmail.com>
2021-03-25 15:36:08 +0100

    edit

--- learning/src/App.vue
+++ learning/src/App.vue
@@ -1,71 +1,40 @@
 <template>
   <div id="app">
     <Header></Header>
-    <AddTodo v-on:add-todo="addTodo"/>
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    
+    <router-view/>
   </div>
 </template>
 
-<script>  
-import Todos from './components/Todos.vue';
+<script>
 import Header from './components/layout/Header.vue';
-import AddTodo from './components/AddTodo.vue';
-import axios from 'axios';
 export default {
-  name: 'App',
-  components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-
-  data () {
-    return {
-      todos:[]
-    }
-  },
-  methods:{
-    deleteTodo(id){
-      this.todos=this.todos.filter(todo=>todo.id !==id);
-    },
-    addTodo(newTodo){
-      const{title, completed} = newTodo;
-      axios.post('https://jsonplaceholder.typicode.com/todos',{
-        title,
-        completed
-      })
-        .then(res => this.todos =[...this.todos, res.data])
-        .catch(err => console.log(err));
-      
-    }
-  },
-  created(){
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-      .then(res => this.todos=res.data) 
-      .catch(err => console.log(err));
+  name:"app",
+  components:{
+    Header
   }
-};
+}
 </script>
 
-<style>
-  *{
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
-  body{
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-  .btn{
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-  .btn:hover{
-    background: #666;
-  }
+}
 </style>
