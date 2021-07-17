0180dab75084b65cfc3df01cdd5f960cc64a5573
El-houcine BRAHIM <houssi27@hotmail.com>
2021-07-17 11:15:01 +0100

    Exception

---
+++ Python/test.py
@@ -0,0 +1,6 @@
+def cap_text(text):
+    '''
+    Input a String
+    Output the capitalize String
+    '''
+    return text.title()
---
+++ Python/test_cap.py
@@ -0,0 +1,13 @@
+import unittest
+import test
+class TestCap(unittest.TestCase):
+    def test_one_word(self):
+        text = 'python'
+        result = test.cap_text(text)
+        self.assertEqual(result, 'Python')
+    def test_multiple_words(self):
+        text = 'monty python'
+        result = test.cap_text(text)
+        self.assertEqual(result, 'Monty Python')
+if __name__ == '__main__':
+    unittest.main()
